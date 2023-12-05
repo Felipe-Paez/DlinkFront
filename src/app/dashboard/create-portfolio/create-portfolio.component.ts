31364bb9371654580cd66f0665f1fe9db7216a2f
Felipe <jpaezforero22@gmail.com>
2023-12-04 19:45:08 -0500

    update-comp

---
+++ src/app/dashboard/create-portfolio/create-portfolio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-portfolio',
+  templateUrl: './create-portfolio.component.html',
+  styleUrls: ['./create-portfolio.component.css']
+})
+export class CreatePortfolioComponent {
+
+}
---
+++ src/app/dashboard/update-portfolio/update-portfolio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-portfolio',
+  templateUrl: './update-portfolio.component.html',
+  styleUrls: ['./update-portfolio.component.css']
+})
+export class UpdatePortfolioComponent {
+
+}
