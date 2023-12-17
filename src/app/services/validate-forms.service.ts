ac86547af3f591f967fef4b1b8551b16b9147523
diazperez430 <diazperez430@gmail.com>
2023-12-17 17:47:07 -0500

    Username added

--- src/app/services/validate-forms.service.ts
+++ src/app/services/validate-forms.service.ts
@@ -9,6 +9,7 @@ export class ValidateFormsService {
   public pass: RegExp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
   public email: RegExp = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 
+
   constructor() { }
 
   // Función de validación personalizada para el campo 'price'
