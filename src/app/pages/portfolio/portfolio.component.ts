6f91edb0d8b3790b087b9302350f55c32dda9d11
Felipe <jpaezforero22@gmail.com>
2023-12-17 13:30:34 -0500

    asd

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -12,13 +12,20 @@ import { PortfolioService } from 'src/app/services/portfolio.service';
 export class PortfolioComponent {
   portfolio: any;
   portfolioName!: string;
+  logged = false;
+  user!:any
 
   constructor(
     private portfolioService:PortfolioService,
     private activatedRoute:ActivatedRoute
     ){}
 
+
   ngOnInit(): void {
+
+    const item = localStorage.getItem("user");    
+    this.user=item ? JSON.parse(item) : null;
+
     this.activatedRoute.params
     .pipe(
       tap( response => {
@@ -39,6 +46,15 @@ export class PortfolioComponent {
       })
       
     })
+
+    if( this.portfolioName == this.user.name)
+  {
+    this.logged = true
   }
+  }
+
+
+  
+  
 
 }
